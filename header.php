1e01abd1b6b9a29fc70722d56ffe7bc1525ce9ca
WallE1602 <sefatulwasi@gmail.com>
2021-12-24 12:42:49 +0600

    deleted garbage

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
   <link href="assets/css/jquery.datetimepicker.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
 
-  <link href="assets/fontawesome/css/all.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
   <link href="assets/DataTables/datatables.min.css" rel="stylesheet">
   <!-- Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
